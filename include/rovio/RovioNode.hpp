7f9b603c8a6082a8237de94e33fbf8a3f714222c
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2024-05-06 23:00:30 +0300

    Fix the frame_id of PCL

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -959,6 +959,7 @@ class RovioNode{
         if(pubPcl_.getNumSubscribers() > 0 || pubMarkers_.getNumSubscribers() > 0 || forcePclPublishing_ || forceMarkersPublishing_){
           pclMsg_.header.seq = msgSeq_;
           pclMsg_.header.stamp = ros::Time(mpFilter_->safe_.t_);
+          pclMsg_.header.frame_id = world_frame_;
           markerMsg_.header.seq = msgSeq_;
           markerMsg_.header.stamp = ros::Time(mpFilter_->safe_.t_);
           markerMsg_.points.clear();
@@ -1067,6 +1068,7 @@ class RovioNode{
         }
         if(pubPatch_.getNumSubscribers() > 0 || forcePatchPublishing_){
           patchMsg_.header.seq = msgSeq_;
+          patchMsg_.header.frame_id = world_frame_;
           patchMsg_.header.stamp = ros::Time(mpFilter_->safe_.t_);
           int offset = 0;
           for (unsigned int i=0;i<mtState::nMax_; i++, offset += patchMsg_.point_step) {
